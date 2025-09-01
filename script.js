ad5d9f7e7e18a53536ea44df6c4cf07653c5a666
Olusanmi Pamilerin kehinde <pamilerinolusanmi@gmail.com>
2025-09-01 16:09:50 +0100

    modals

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 const modal = document.querySelectorAll(".modal");
 const button = document.querySelectorAll(".view");
 const span = document.querySelectorAll(".close");
-button.onclick = function () {
-  modal.style.display = "block";
-};
-span.onclick = function () {
-  modal.style.display = "none";
-};
+
+for (let i = 0; i < button.length; i++) {
+  button[i].onclick = function () {
+    modal[i].style.display = "block";
+  };
+  span[i].onclick = function () {
+    modal[i].style.display = "none";
+  };
+}
+
 window.onclick = function (event) {
   if (event.target === modal) {
     modal.style.display = "none";
