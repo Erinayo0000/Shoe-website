d23db1196157d99533a5a5cb27e7301bcae1a312
Olusanmi Pamilerin kehinde <pamilerinolusanmi@gmail.com>
2025-09-01 15:35:25 +0100

    Started working on the modal

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-"use script;";
+"use strict";
+const modal = document.querySelectorAll(".modal");
+const button = document.querySelectorAll(".view");
+const span = document.querySelectorAll(".close");
+button.onclick = function () {
+  modal.style.display = "block";
+};
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
